3b025342fc5fad8aefa824a310752cd2fd03fe05
文静 <kulta@bunseis-MacBook-Air.local>
2022-11-26 22:35:01 +0200

    edit comment

--- client/src/components/AddClient.js
+++ client/src/components/AddClient.js
@@ -9,21 +9,21 @@ export default function AddClientModal() {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [phone, setPhone] = useState('')
-    // const [addClient] = useMutation(ADD_CLIENT, {
-    //     variables: { name: name, email: email, phone: phone },
-    //     refetchQueries: [{ query: GET_CLIENTS }],
-    // })
     const [addClient] = useMutation(ADD_CLIENT, {
-        variables: { name, email, phone },
-        update(cache, { data: { addClient } }) {
-            const { clients } = cache.readQuery({ query: GET_CLIENTS })
-
-            cache.writeQuery({
-                query: GET_CLIENTS,
-                data: { clients: [...clients, addClient] },
-            })
-        },
+        variables: { name: name, email: email, phone: phone },
+        refetchQueries: [{ query: GET_CLIENTS }],
     })
+    // const [addClient] = useMutation(ADD_CLIENT, {
+    //     variables: { name, email, phone },
+    //     update(cache, { data: { addClient } }) {
+    //         const { clients } = cache.readQuery({ query: GET_CLIENTS })
+
+    //         cache.writeQuery({
+    //             query: GET_CLIENTS,
+    //             data: { clients: [...clients, addClient] },
+    //         })
+    //     },
+    // })
 
     const onSubmit = (e) => {
         e.preventDefault()
