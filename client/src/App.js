e7e61cdc7dc12b416134436af68c87306f345041
文静 <ruhuiwen0608@gmail.com>
2022-12-01 16:34:03 +0200

    relative url at the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import Header from './components/Header'
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+import {
+    ApolloProvider,
+    ApolloClient,
+    InMemoryCache,
+    HttpLink,
+} from '@apollo/client'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import Project from './pages/Project'
 
+// const client = new ApolloClient({
+//     uri: 'http://localhost:8000/graphql',
+//     cache: new InMemoryCache(),
+// })
+
 const client = new ApolloClient({
-    uri: '/graphql',
+    link: new HttpLink({ uri: '/graphql' }),
     cache: new InMemoryCache(),
 })
 
