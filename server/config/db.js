b942ae4bd3c97a623a2c8798e3b2050a72f1db85
文静 <ruhuiwen0608@gmail.com>
2022-12-01 17:11:30 +0200

    .

--- server/config/db.js
+++ server/config/db.js
@@ -2,18 +2,17 @@ const mongoose = require('mongoose')
 
 const url = process.env.MONGO_URI
 
-// const connectDB = async () => {
-//     const conn = await mongoose.connect(process.env.MONGO_URI)
-//     console.log(
-//         `MongoDB connected: ${conn.connection.host}`.cyan.underline.bold //cyan.underline.bold is the 'colors' package installed
-//     )
-// }
-
 mongoose
     .connect(url)
     .then((res) => console.log('Connected to MongoDB'))
     .catch((err) =>
         console.log('Error occured when connecting to MongoDB: ', err)
     )
+// const connectDB = async () => {
+//     const conn = await mongoose.connect(process.env.MONGO_URI)
+//     console.log(
+//         `MongoDB connected: ${conn.connection.host}`.cyan.underline.bold //cyan.underline.bold is the 'colors' package installed
+//     )
+// }
 
 // module.exports = connectDB
