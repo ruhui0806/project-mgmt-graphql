06d332c2594efff120033161007e81daa11a5663
文静 <ruhuiwen0608@gmail.com>
2022-12-02 11:37:00 +0200

    delete comments

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose')
 const colors = require('colors')
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
-// const connectDB = require('./config/db')
+const { ApolloServer, gql } = require('apollo-server-express')
+
 const port = process.env.PORT || 8000
 
 const app = express()
@@ -13,7 +14,6 @@ app.use(cors())
 app.use(express.static('build'))
 
 //connect to database
-
 const url = process.env.MONGO_URI
 
 mongoose
@@ -30,13 +30,6 @@ app.use(
         graphiql: process.env.NODE_ENV === 'development',
     })
 )
-// app.use(
-//     '/graphql',
-//     graphqlHTTP({
-//         schema,
-//         graphiql: process.env.NODE_ENV === 'development',
-//     })
-// )
-//graphiql only set to be true if process.env.NODE_ENV === 'development' is true;
+// graphiql only set to be true if process.env.NODE_ENV === 'development' is true;
 
 app.listen(port, console.log(`server running on ${port}`))
