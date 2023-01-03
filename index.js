bf626152d5ec07c0b9d55c79f3f988d44e9ab5a1
文静 <ruhuiwen0608@gmail.com>
2023-01-03 19:04:13 +0200

    edit server/index.js file

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ app.use(
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    // })
 }
 app.listen(port, console.log(`server running on ${port}`))
