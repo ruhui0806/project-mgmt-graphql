43813a1f1d792f67f9f6478755fcacc1cf48b75f
文静 <ruhuiwen0608@gmail.com>
2023-01-03 19:16:16 +0200

    link -> uri in client/App.js file

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(
 )
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+    app.use(express.static('build'))
     // app.get('*', (req, res) => {
     //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     // })
