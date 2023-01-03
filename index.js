4b348527cf3b02ced4746d62783b4de07723e8fd
文静 <ruhuiwen0608@gmail.com>
2023-01-03 19:27:18 +0200

    reggenerate build

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ app.use(
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('build'))
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    // })
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    })
 }
 app.listen(port, console.log(`server running on ${port}`))
