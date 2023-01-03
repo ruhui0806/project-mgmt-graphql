2afb91418943b09438a5db449f0fc7baad891550
文静 <ruhuiwen0608@gmail.com>
2023-01-03 15:42:04 +0200

    update the static folder

--- index.js
+++ index.js
@@ -30,9 +30,6 @@ app.use(
 )
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+    app.use(express.static('client/build'))
 }
 app.listen(port, console.log(`server running on ${port}`))
